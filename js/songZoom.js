4bbdf67ce86c1b650b4b2c1a1705e847f2c990b4
resetmerlin <108568153+resetmerlin@users.noreply.github.com>
2022-11-03 10:18:00 +0900

    2022/11/03

---
+++ js/songZoom.js
@@ -0,0 +1,22 @@
+const selectSong = document.querySelector(".main__song-default");
+selectSong.addEventListener("click", () => {
+  const fullBlackScreen = document.createElement("div");
+  fullBlackScreen.style = "position:fixed";
+
+  fullBlackScreen.style = "z-index:1000";
+  fullBlackScreen.style.background = "black";
+  fullBlackScreen.classList.add("fullBlack");
+  const main = document.querySelector(".main");
+
+  main.appendChild(fullBlackScreen);
+  const mainSong = document.createElement("div");
+  mainSong.style = "position:fixed";
+
+  mainSong.style = "z-index:1001";
+  mainSong.style =
+    "background-image:linear-gradient(to right bottom, rgb(187 187 187 / 80%), rgb(64 64 64 / 80%));";
+  mainSong.classList.add("mainSong");
+  fullBlackScreen.appendChild(mainSong);
+
+  const mainSongImg = document.createElement("img");
+});
